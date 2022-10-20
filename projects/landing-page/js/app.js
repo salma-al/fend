c404b3f688cd2cb6504d40f6d79aa146a02acb49
salma-al <salma_fouad@outlook.com>
2022-10-20 10:14:18 +0200

    defined global variables in JS file

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -1,62 +1,60 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
- * 
-*/
-
+ *
+ */
+const navbarUl = document.querySelector('#navbar__list');
+const sectionId = document.querySelector('#section1');
+const sectionTitles = sectionId.dataset.nav;
+// const sections = document.querySelectorAll('[data-nav]');
+const activeClass = document.querySelector('.your-active-class');
+const sections = document.querySelectorAll('section');
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
-
-
+ *
+ */
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
 
-
 // Add class 'active' to section when near top of viewport
 
-
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
