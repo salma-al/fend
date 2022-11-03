645d376f9a0df91c8f43da68843d8e42c1d5094e
salma-al <salma_fouad@outlook.com>
2022-11-03 20:13:18 +0200

    changes in app and server js

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -2,18 +2,58 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
+/* Dependencies */
+const bodyParser = require('body-parser');
 /* Middleware*/
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8000;
+
+// Spin up the server
+const server = app.listen(port, () => {
+  // Callback to debug
+  console.log(`localhost: ${port}`);
+});
+
+// Callback function to complete GET '/all'
+app.get('/all', function (req, res) {
+  res.send(projectData);
+  console.log(projectData);
+});
+
+// // post method
+// app.post('/add', function (req, res) {
+//   projectData = req.body;
+//   res.send(projectData);
+// });
+
+// post route
+const contentArea = [];
+
+app.post('/postData', (req, res) => {
+  newData = {
+    date: req.body.date,
+    temp: req.body.temp,
+    feelings: req.body.feelings,
+  };
+  contentArea.push(newData);
+  console.log(contentArea);
+  //   projectData = newData;
+  res.send(projectData);
+});
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,71 @@
+// base url
+let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+// Personal API Key for OpenWeatherMap API
+let apiKey = '&appid=fac48d14a5f14c4a5fc1d6745ae486b3&units=imperial';
+
 /* Global Variables */
+const btn = document.querySelector('#generate');
+const date = document.querySelector('#date');
+const temp = document.querySelector('#temp');
+const content = document.querySelector('#content');
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+btn.addEventListener('click', () => {
+  //   e.preventDefault();
+  const zip = document.querySelector('#zip').value;
+  let feelings = document.querySelector('#feelings').value;
+  //   const getUrl = baseURL + zip + apiKey;
+  //   getData(getUrl).then((data) => console.log(data));
+  getData(baseURL, zip, apiKey)
+    .then((data) => {
+      postData('/postData', {
+        date: newDate,
+        temp: data.main.temp,
+        feelings: feelings,
+      });
+    })
+    .then(updateUI());
+});
+
+const getData = async (baseURL, zip, apiKey) => {
+  const res = await fetch(baseURL + zip + apiKey);
+  try {
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+const postData = async (url = '', data = {}) => {
+  const res = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await res.json();
+    console.log(newData);
+    return newData;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+const updateUI = async () => {
+  const req = await fetch('/all');
+  try {
+    const allData = await req.json();
+    date.innerHTML = allData.date;
+    temp.innerHTML = allData.temp;
+    content.innerHTML = allData.feelings;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
